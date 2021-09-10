a733a7a25c9d879246ba3aeef903aa8612a086c0
vladi <vladzar@krem.digital>
2021-09-10 14:48:38 +0400

    First push

---
+++ Assets/Code/DestroyChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyChecker : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private Transform destroyedPrefab;
+    [SerializeField] private float maxMagnitude = 4.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var vel = (other.rigidbody ? other.rigidbody.velocity : Vector3.up*2) - rb.velocity;
+
+        if (vel.magnitude >= maxMagnitude)
+        {
+            Instantiate(destroyedPrefab, transform.position, transform.rotation * Quaternion.Euler(90, 0, 0));
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
---
+++ Assets/Code/Destroyer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    const String TAG_DESTRUCT = "can_destroyed";
+    private Camera m_MainCamera;
+    
+    // ---
+    public Transform prefab;
+
+    private void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                if (hit.collider.CompareTag(TAG_DESTRUCT))
+                {
+                    Instantiate(prefab, hit.transform.position, hit.transform.rotation * Quaternion.Euler(new Vector3(90, 0, 0)));
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
